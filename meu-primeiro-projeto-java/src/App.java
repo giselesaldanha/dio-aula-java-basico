dc10ce9c71e5c7c5118cd15f38a80c958ef3b227
giselesaldanha <giselesaldanha@yahoo.com.br>
2023-09-14 16:54:53 -0300

    Retirando texto de Teste de Commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá,turma DIO!");
-        System.out.println("Teste de Comitt");
     }
 }
