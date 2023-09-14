6386da3b836d9091debfedfe1d9822b634a20a0f
giselesaldanha <giselesaldanha@yahoo.com.br>
2023-09-14 16:53:29 -0300

    Teste de Commit 2

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá,turma DIO!");
+        System.out.println("Teste de Comitt");
     }
 }
